f3622051a8628a00186c5ac8edccdca0042bcf9a
fazi1live <faziislive@gmail.com>
2021-09-12 21:29:24 +0500

    Final Changes made

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Package = require('../package.json');
 
-mongoose.connect("mongodb+srv://arbaz:arbaz2001@southfittedwardrobesand.6ikfx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true, }, function (error) {
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true, }, function (error) {
     if (!error) {
         console.log('\nMogoDb Connected Successfuly at 27017 with Database Name South Fitted Wardrobes & Kitchen\n');
         console.log('Your App Has the Following Dependicies\n');
